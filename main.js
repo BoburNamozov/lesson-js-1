da62a3e1233e8956b83d46e84ae4b8f8353f8c0d
BoburNamozov <bnamozov23@gmail.com>
2024-03-20 16:37:45 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,71 @@
+ 
+// let a = "Salom"
+
+// console.log(a);
+
+// alert("hi")
+
+// let a = "bobur"
+
+// console.log (a);
+// var a = "MuxammadSodiq"
+// a = "Mumtozbegim"
+// a = "Boburga"
+
+
+
+
+  
+// let name = "Bobur"
+// let b = "Shoxiniso"
+// let g = "Mumtozbegim"0
+// let h = "we are rich"
+//  console.log(a);
+//  console.info(a)
+//  console.error(a)
+//  console.warn(a)
+//  console.warn(a)
+
+
+
+
+
+   var name = "Rich"
+   var admin
+
+   admin=name
+
+   console.log(admin);
+
+
+   // let a = undefined
+   // console.log(b);
+   // boolean = true false
+
+   // console.log(
+   //  "salom" * "alik"
+   // );
+   // console.log(
+   //    2*10
+   // ); 
+   // let  a = "10"
+   // let  b =1
+  
+   //  
+   // console.log(
+      // a + b,
+      // a - b,
+      // a * b, 
+      // a / b 
+      // a === b
+      // a % b
+      // a 
+   
+   // console.log(
+      // typeof(a)
+   
+   // let userName = "b"
+
+   //  let name = "Bobur"
+
+   // console.log("name");
